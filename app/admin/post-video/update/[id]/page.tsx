3cb7438e85b6cb76d64e08edb85342c32373925a
Raihanbasyir <rayhanbasyir@gmail.com>
2025-07-07 07:58:00 +0700

    fix deploy problem 2

--- app/admin/post-video/update/[id]/page.tsx
+++ app/admin/post-video/update/[id]/page.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import { notFound } from "next/navigation";
 
-const Page = async ({params} : {params : {id : string}}) => {
-    const id = params.id ;
+const Page = async ({params} : {params : Promise<{id : string}>}) => {
+    const id = (await params).id ;
     const dataUpdatePostVideo = await getPostVideoById(id);
 
     if(!dataUpdatePostVideo){
