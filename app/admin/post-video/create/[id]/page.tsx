32bc623265778633a04c939896eec26188db3771
Raihanbasyir <rayhanbasyir@gmail.com>
2025-07-07 07:53:03 +0700

    fix problem deploy

--- app/admin/post-video/create/[id]/page.tsx
+++ app/admin/post-video/create/[id]/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 
-const CreatePostVideoPage = async ({params} : {params : {id : string}}) => {
-    const id = params.id
+const CreatePostVideoPage = async ({params} : {params : Promise<{id : string}>}) => {
+    const id = (await params).id
     return (
         <>
         <Button className="w-fit border border-[#F8F8FF] bg-[#25388C] text-base font-medium text-white hover:text-black hover:bg-[#F8F8FF]" asChild>
