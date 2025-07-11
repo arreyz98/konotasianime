79d71d7ff8ad5a9e386a4c712790d82e8df9d907
Raihanbasyir <rayhanbasyir@gmail.com>
2025-07-12 01:50:25 +0700

    fix params 5

--- app/api/videos/[id]/route.ts
+++ app/api/videos/[id]/route.ts
@@ -1,18 +1,20 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: Promise<{ id: string }> }) {
+  const id  = (await params).id
   const { title, deskripsi, linkVideo, duration, episode } = await req.json()
 
   const updated = await prisma.postVideo.update({
-    where: { id: params.id },
+    where: { id: id },
     data: { title, deskripsi, linkVideo, duration, episode },
   })
 
   return NextResponse.json(updated)
 }
 
-export async function DELETE(_: Request, { params }: { params: { id: string } }) {
-  await prisma.postVideo.delete({ where: { id: params.id } })
+export async function DELETE(_: Request, { params }: { params: Promise<{ id: string }> }) {
+  const id = (await params).id
+  await prisma.postVideo.delete({ where: { id: id } })
   return NextResponse.json({ message: 'Video dihapus' })
 }
