5f5daf8b248db20fccf003f83c45b93cb0bd1409
Raihanbasyir <rayhanbasyir@gmail.com>
2025-09-20 06:41:29 +0700

    fix update sidebar & tampilan mobile 1

--- app/anime/search/page.tsx
+++ app/anime/search/page.tsx
@@ -1,7 +1,12 @@
 import AnimeGrid from '@/components/AnimeGrid'
 
-export default function SearchPage({ searchParams }: { searchParams?: { q?: string } }) {
-  const query = searchParams?.q ?? '' // masih bisa ada q, tapi optional
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams?: Promise<{ q?: string }>
+}) {
+  const resolved = searchParams ? await searchParams : {}
+  const query = resolved.q ?? ''
 
   return (
     <section className="min-h-screen px-4 sm:px-8 md:px-12 lg:px-20 xl:px-24 py-6 bg-[#1C2029]">
